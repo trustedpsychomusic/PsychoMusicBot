a2f9023330849d1213f142273a16f2e4c929ba5e
ARYANSONIOP2 <soniaryan2240025@gmail.com>
2025-09-29 13:26:42 +0530

    Update Youtube.py

--- BrandrdXMusic/platforms/Youtube.py
+++ BrandrdXMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from InflexMusic.utils.database import is_on_off
-from InflexMusic.utils.formatters import time_to_seconds
+from BrandrdXMusic.utils.database import is_on_off
+from BrandrdXMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
